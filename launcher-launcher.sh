a8a12356cee0983432ba83477ec1b3eb52743a8b
Atul Varma <varmaa@gmail.com>
2017-03-10 09:29:19 -0500

    Document launcher-launcher and sub-launcher.

--- launcher-launcher.sh
+++ launcher-launcher.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+# This just wraps whatever the Docker host wants to run in the
+# Diego buildpack lifecycle launcher, which sets up some environment
+# variables and makes buildpack binaries accessible to us.
+#
+# The Diego launcher only seems to like running a single command in a
+# specific home directory, though, so we're going to store our
+# execution parameters in environment variables and restore them
+# after the Diego launcher does its job.
+
 export ORIGINAL_ARGS=$@
 export ORIGINAL_CWD=$(pwd)
 
--- sub-launcher.sh
+++ sub-launcher.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+# For details on what these environment variables contain, see
+# launcher-launcher.sh.
+
 cd $ORIGINAL_CWD
 
 exec $ORIGINAL_ARGS
