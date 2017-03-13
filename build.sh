7c598fafe2220466b8460d5020d72ce0f059e103
M. Adam Kendall <michael.kendall@gsa.gov>
2017-03-13 09:43:48 -0400

    Tag image with latest and proper version

--- build.sh
+++ build.sh
@@ -25,8 +25,11 @@ fi
 LANGUAGE=$1
 GO_VERSION="1.7"
 DIEGO_VERSION=$(curl -s -L https://api.github.com/repos/cloudfoundry/diego-release/releases/latest | jq -r .tag_name)
+BP_VERSION=$(curl -s -L https://api.github.com/repos/cloudfoundry/${LANGUAGE}-buildpack/releases/latest | jq -r .tag_name)
 
-docker build . --tag cloud-gov/${LANGUAGE}:latest \
+docker build . \
+    --tag cloud-gov/${LANGUAGE}:${BP_VERSION} \
+    --tag cloud-gov/${LANGUAGE}:latest \
     --build-arg GO_VERSION=${GO_VERSION} \
     --build-arg DIEGO_VERSION=${DIEGO_VERSION} \
     --build-arg LANGUAGE=${LANGUAGE}
