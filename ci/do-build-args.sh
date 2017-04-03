9daf37cd5e247677c5bdf8082df38bbce7093f2e
M. Adam Kendall <michael.kendall@gsa.gov>
2017-04-03 16:35:10 -0400

    Use docker-in-docker image

--- ci/do-build-args.sh
+++ ci/do-build-args.sh
@@ -3,19 +3,14 @@
 set -e
 set -u
 
-source /opt/resource/common.sh
-
-# for jq
-export PATH=/usr/local/bin:$PATH
-
-start_docker
-
 SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+# get jq
+apt-get update -qq && apt-get install -qqy jq
 
 BP_VERSION=$(curl -s -L http://bosh.io/api/v1/releases/github.com/cloudfoundry/${LANGUAGE}-buildpack-release -H "Content-type: application/json" -H "Accept: application/json" | jq -r '.[0] | .version')
 
-(cd "${SCRIPTPATH}/../
-  ./build.sh" "${LANGUAGE}")
+(cd "${SCRIPTPATH}"/../ && ./build.sh "${LANGUAGE}")
 
 docker run -d \
   --publish 5000:5000 \
