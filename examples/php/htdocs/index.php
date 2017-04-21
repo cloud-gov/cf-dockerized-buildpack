2256a2d0f0752513da305368b0141308d9aa3d14
M. Adam Kendall <michael.kendall@gsa.gov>
2017-04-21 17:30:43 -0400

    Add php example

---
+++ examples/php/htdocs/index.php
@@ -0,0 +1,24 @@
+<?php
+  require('hello-world.php');
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>PHP Simple Example</title>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="">
+    <link rel="shortcut icon" href="">
+  </head>
+
+  <body>
+    <h1><?php print helloWorld(); ?></h1>
+    <p>Click <a href="/info.php">here to view PHP information</a></p>
+  </body>
+
+</html>
