c0124335dac3f8a4dbf89125110784d8237cdaff
M. Adam Kendall <michael.kendall@gsa.gov>
2017-05-16 11:25:47 -0400

    Try running tests in bash

--- ci/test-example-app.sh
+++ ci/test-example-app.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 set -u
-set -x
 
 SCRIPTPATH=$( cd "$(dirname "$0")" ; pwd -P )
+MAX_ATTEMPTS=10
+INTERVAL_SECONDS=5
 
 # shellcheck disable=SC1091
 . /opt/resource/common.sh
@@ -16,13 +17,32 @@ mkdir -p /usr/share/ssl/certs
 rm -rf /usr/share/ssl/certs/ca-bundle.crt
 cat /usr/share/ca-certificates/mozilla/* >> /usr/share/ssl/certs/ca-bundle.crt
 
-mkdir -p /usr/local/bin
-curl -sL "https://github.com/docker/compose/releases/download/1.13.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -sL --fail https://github.com/docker/compose/releases/download/1.13.0/run.sh > /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # start up docker
 export PORT=2375
 start_docker "" ""
 
-cd "$SCRIPTPATH/.." || exit
-python manage_examples.py test "$LANGUAGE"
+cd "$SCRIPTPATH/../examples/$LANGUAGE" || exit
+docker-compose build
+( docker-compose up & )
+
+ATTEMPTS=1
+while true; do
+  echo -n "Checking if $LANGUAGE server works... "
+  if curl -sL http://127.0.0.1:8080/ | grep -i 'hello world' > /dev/null 2>&1; then
+    echo "[YES]"
+    break
+  else
+    echo "[retrying]"
+  fi
+  ATTEMPTS=$((ATTEMPTS + 1))
+  if [ $ATTEMPTS -gt $MAX_ATTEMPTS ]; then
+    echo "Failed after $MAX_ATTEMPTS attempts"
+    exit 1
+  fi
+  sleep $INTERVAL_SECONDS
+done
+
+docker-compose down -v
